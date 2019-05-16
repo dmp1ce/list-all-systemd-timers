1aaccb56bb39739305d103ec5f643c12b8a8ee12
David Parrish <daveparrish@tutanota.com>
2019-05-16 18:37:25 -0400

    Add shellcheck suggestions for bash script

--- list-all-systemd-timers.bash
+++ list-all-systemd-timers.bash
@@ -2,7 +2,7 @@
 
 # Print all systemd timers, including system-wide and all user timers.
 
-if [ $(id -u) -ne 0 ]; then
+if [ "$(id -u)" -ne 0 ]; then
   echo "Sorry, not root user."
   echo "Please use sudo or su to become root user before running this script."
   exit 1
@@ -15,9 +15,9 @@ usersWithActiveDbusDaemon=$(systemctl\
  --no-legend --full list-units --state=active 'user@*.service' |\
  awk '{ print substr($1, 6, length($1) - length(".service") - 5) }')
 
-for user_uid in $usersWithActiveDbusDaemon; do 
-  username=$(id -nu $user_uid)
+for user_uid in $usersWithActiveDbusDaemon; do
+  username=$(id -nu "$user_uid")
   echo ""
   echo "Timers for $username:"
-  su $username -c 'XDG_RUNTIME_DIR=/run/user/'$user_uid' systemctl --user list-timers'
+  su "$username" -c 'XDG_RUNTIME_DIR=/run/user/'"$user_uid"' systemctl --user list-timers'
 done
